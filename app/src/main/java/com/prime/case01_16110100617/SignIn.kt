5b36e238e7e533c5f93fb2e6fd1f6dd33dca82bc
lzyprime <2383518170@qq.com>
2018-09-19 09:22:37 +0800

    login_activity theme-->parent=base.apptheme 显示正常

---
+++ app/src/main/java/com/prime/case01_16110100617/SignIn.kt
@@ -0,0 +1,12 @@
+package com.prime.case01_16110100617
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SignIn : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
